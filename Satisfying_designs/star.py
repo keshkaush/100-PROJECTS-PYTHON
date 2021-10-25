bdd60a9f304828df7085b801bfdac2d38da908d6
keshkaush <keshav.kaushik344@gmail.com>
2021-10-25 11:11:07 +0530

    design file #1

---
+++ Satisfying_designs/star.py
@@ -0,0 +1,12 @@
+import turtle
+from turtle import *
+color('red', 'yellow')
+begin_fill()
+turtle.speed(0)
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
